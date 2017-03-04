8b2fb2928252ba9800b47e5847f906908fb9bfc1
Christopher Plain <christopher@jetcitycoder.com>
2017-03-04 12:59:48 -0800

    Implement creation of new resources

--- spec/controllers/resources_controller_spec.rb
+++ spec/controllers/resources_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe ResourcesController, type: :controller do
-
+  describe 'POST resources#create' do
+    it 'should create a new resource in the database' do
+      post :create, params: { resource: { name: 'Name', description: 'Description' } }
+      expect(response).to have_http_status(:success)
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value['name']).to eq('Name')
+      expect(response_value['description']).to eq('Description')
+      expect(Resource.last.name).to eq('Name')
+    end
+  end
 end
