437751cc15f45eaa3ccdba25f7e8308f07f29660
Christopher Plain <christopher@jetcitycoder.com>
2017-03-04 21:24:39 -0800

    Add categories controller and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304055312) do
+ActiveRecord::Schema.define(version: 20170305041832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "parent_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["parent_id"], name: "index_categories_on_parent_id", using: :btree
+  end
 
   create_table "resources", force: :cascade do |t|
     t.string   "name"
