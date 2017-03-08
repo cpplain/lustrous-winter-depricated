a4ce229d883a14b5f96866946c91f63c520ad69b
Christopher Plain <christopher@jetcitycoder.com>
2017-03-07 17:19:54 -0800

    Create subjects via subjects#create

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,2 +1,18 @@
 class SubjectsController < ApplicationController
+  def create
+    parent = Subject.find_by_id(subject_params[:parent])
+    subject = Subject.create(name: subject_params[:name], parent: parent)
+    return render json: subject, status: :created if subject.valid?
+    render json: render_errors(subject), status: :unprocessable_entity
+  end
+
+  private
+
+  def subject_params
+    params.require(:subject).permit(:name, :parent)
+  end
+
+  def render_errors(subject)
+    { errors: subject.errors }
+  end
 end
