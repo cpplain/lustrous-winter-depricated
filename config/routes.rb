86f15b91c1bf21704fb4c3009976bad78fad8de5
Christopher Plain <christopher@jetcitycoder.com>
2017-03-09 09:51:38 -0800

    Show resources via resources#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :resources, only: [:index, :create, :update, :destroy]
+  resources :resources, only: [:index, :create, :show, :update, :destroy]
   resources :subjects, only: [:index, :create, :update, :destroy]
 end
