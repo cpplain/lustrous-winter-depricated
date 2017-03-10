147f6171f72ba0726df304637b408477a6e8880c
Christopher Plain <christopher@jetcitycoder.com>
2017-03-10 15:22:57 -0800

    Add ratings controller and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170310230516) do
+ActiveRecord::Schema.define(version: 20170310231427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 20170310230516) do
     t.string   "state"
     t.string   "country"
     t.boolean  "free"
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.boolean  "recommend"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "resources", force: :cascade do |t|
