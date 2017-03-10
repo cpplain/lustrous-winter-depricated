55b280eb1cfb5ea1d839c7f60dc378c33ba6c02c
Christopher Plain <christopher@jetcitycoder.com>
2017-03-10 15:12:54 -0800

    Add reviews controller and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170310223516) do
+ActiveRecord::Schema.define(version: 20170310230516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,12 @@ ActiveRecord::Schema.define(version: 20170310223516) do
     t.datetime "updated_at",  null: false
     t.integer  "subject_id"
     t.index ["subject_id"], name: "index_resources_on_subject_id", using: :btree
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "subjects", force: :cascade do |t|
