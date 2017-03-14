12e8e2a63ef9879b5f337b962f36a923ad31ac78
Christopher Plain <christopher@jetcitycoder.com>
2017-03-14 10:11:51 -0700

    Add columns to resources

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,9 +1,10 @@
 class Resource < ApplicationRecord
   audited
   belongs_to :subject
-  has_many :media
   has_many :ratings, as: :morphic_rating
   has_many :reviews
   validates :name, presence: true
   validates :description, presence: true
+  validates :medium, presence: true
+  validates :free, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314162619) do
+ActiveRecord::Schema.define(version: 20170314170001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,9 +49,18 @@ ActiveRecord::Schema.define(version: 20170314162619) do
   create_table "resources", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.integer  "subject_id"
+    t.string   "medium"
+    t.string   "author"
+    t.string   "isbn"
+    t.string   "organization"
+    t.string   "url"
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
+    t.boolean  "free"
     t.index ["subject_id"], name: "index_resources_on_subject_id", using: :btree
   end
 
