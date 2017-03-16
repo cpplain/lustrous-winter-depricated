8f61133cab8318f25847f31fe7c334e2ad8c1c73
Christopher Plain <christopher@jetcitycoder.com>
2017-03-15 20:41:58 -0700

    Add City/State/Country models, associations, and controllers

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -6,6 +6,8 @@ class Resource < ApplicationRecord
   has_many :ratings, as: :morphic_rating
   has_many :resource_authors
   has_many :authors, through: :resource_authors
+  has_many :locations
+  has_many :cities, through: :locations
   validates :name, presence: true
   validates :description, presence: true
   validates :medium, presence: true
