1c44e10609739c3053ebaeed940fbfecae408956
Christopher Plain <christopher@jetcitycoder.com>
2017-03-16 17:16:39 -0700

    View all authors via authors#index

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,5 @@
 class AuthorsController < ApplicationController
+  def index
+    render_success(:ok, Author.order(last: :asc, first: :asc, middle: :asc))
+  end
 end
--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe AuthorsController, type: :controller do
+  describe 'GET authors#index' do
+    before do
+      @author1 = FactoryGirl.create(:author)
+      @author2 = FactoryGirl.create(:author)
+      get :index
+      json = JSON.parse(response.body)
+      @author_ids = json.collect { |author| author['id'] }
+    end
 
+    it 'returns all authors in the response body' do
+      expect(@author_ids).to eq([@author1.id, @author2.id])
+    end
+
+    it 'returns HTTP status ok' do
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
